2b6506c6fced9c2e62f281afd65d95fab2c37db4
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-06-13 18:59:10 +0530

    added A_Insomnia_cure.cpp file

---
+++ A_Insomnia_cure.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define for2(i, a, b) for (int i = a; i < b; i++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve() {
+    int k,l,m,n,d;
+    cin>>k>>l>>m>>n>>d;
+    int cnt=0;
+    for1(i, d){
+        if(i%k==0 or i%l==0 or i%m==0 or i%n==0)
+        cnt++;
+    }
+    cout<<cnt;
+}
+
+signed main() {
+    fast
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
