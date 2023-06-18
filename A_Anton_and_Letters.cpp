673c29bbb1118f98de7ea2fe1a3c73936b74c4a5
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-06-18 23:41:55 +0530

    Added file of day 17b june

---
+++ A_Anton_and_Letters.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define for2(i, a, b) for (int i = a; i < b; i++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve() {
+    string str;
+    cin>>str;
+    set<char>st;
+    for0(i, str.length()){
+        st.insert(str[i]);
+    }
+    for(auto x:st){
+        cout<<x<<endl;
+    }
+
+    cout<<str.length()<<endl;
+}
+
+signed main() {
+    fast
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
---
+++ Append_for_OR.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define for2(i, a, b) for (int i = a; i < b; i++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve() {
+    int n,y;
+    cin>>n>>y;
+    int var=0;
+    for0(i,n){
+        int x;
+        cin>>x;
+        var |=x;
+    }
+    int a=y-var;
+    if((var|a)==y) cout<<a<<endl;
+    else
+    cout<<-1<<endl;
+    
+    
+}
+
+signed main() {
+    fast
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
