ef1eb20f6dc2f3e541edbc8418f6569c851a91cd
Abhishek Dethe <abhishekdethe@gmail.com>
2023-06-21 23:32:17 +0530

    added files of 21  june

---
+++ Donation_Rewards.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define for2(i, a, b) for (int i = a; i < b; i++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve() {
+    int x;
+    cin>>x;
+    if(x<=3) cout<<"BRONZE"<<endl;
+    else if(x>3 and x<=6) cout<<"SILVER"<<endl;
+    else cout<<"GOLD"<<endl;
+}
+
+signed main() {
+    fast
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
