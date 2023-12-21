fc992253486c352ab43a0fd964e674ad9c37db4c
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-12-22 01:10:27 +0530

    new problem added

---
+++ A_Problemsolving_Log.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define for2(i, a, b) for (int i = a; i < b; i++)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve() {
+    int n;
+    cin >> n;
+    map <char, int> mp;
+    for(int i=0; i<n; i++){
+        char x;
+        cin >> x;
+        mp[x]++;
+    }
+    int cnt = 0;
+    for(int i='A'; i<='Z'; i++){
+        if(mp[i] >= i-'A'+1) cnt++;
+    }
+    cout << cnt << endl;
+}
+
+signed main() {
+    fast
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
