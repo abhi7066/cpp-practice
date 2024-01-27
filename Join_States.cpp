5ee0a95ebea6781ce55f0078dd6a52f874578ac8
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-01-27 20:21:03 +0530

    codechef start 118

--- Join_States.cpp
+++ Join_States.cpp
@@ -3,41 +3,21 @@ using namespace std;
 
 int main()
 {
-    // your code goes here
     int t;
     cin >> t;
-    while (t--){
-        int n, m;
+    while (t--)
+    {
+        int n, m, sum = 0, cnt = 0;
         cin >> n >> m;
-        vector<int> arr;
+        int arr[n];
         for (int i = 0; i < n; i++){
-            int x;
-            cin >> x;
-            arr.push_back(x);
-        }
-
-        vector<int> arr1;
-        for (int i = 0; i < n; i++){
-            if (arr[i] < m and i + 1 != n){
-                arr1.push_back(arr[i] + arr[i + 1]);
-                i++;
-            }
-            else{
-                arr1.push_back(arr[i]);
+            cin >> arr[i];
+            sum+=arr[i];
+            if(sum>=m){
+                cnt++;
+                sum=0;
             }
         }
-
-        vector<int> arr2;
-        for (int i = arr1.size()-1; i >= 0; i--){
-            if (arr1[i] < m and i - 1 >= 0){
-                arr2.push_back(arr1[i] + arr1[i - 1]);
-                i--;
-            }
-            else{
-                arr2.push_back(arr1[i]);
-            }
-        }
-
-        cout << arr2.size() << endl;
+        cout<< cnt << endl;
     }
 }
