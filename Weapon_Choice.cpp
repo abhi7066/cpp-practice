26887d2b76a28171a2b95fcaae0de47988c8371d
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-01-29 13:04:44 +0530

    codechef start 117

--- Weapon_Choice.cpp
+++ Weapon_Choice.cpp
@@ -6,9 +6,15 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-        int health, gun, laserBefore, laserAfter, atleastHit;
+        int health, gun, laserBefore, laserAfter, atleastHit, laserHit, gunHit;
         cin >> health >> gun >> laserBefore >> laserAfter >> atleastHit;
-        int gunPower, laserPower;
-        
+        gunHit = (health+gun-1)/gun;
+        if(laserBefore*atleastHit >= health){
+            laserHit = (health+laserBefore-1)/laserBefore;
+        }
+        else{
+            laserHit = atleastHit + (health - laserBefore*atleastHit+laserAfter-1)/laserAfter;
+        }
+        cout << min(gunHit, laserHit) << endl;
     }
 }
