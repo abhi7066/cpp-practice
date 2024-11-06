050cfcdb143b300a3ccf4c3d4e7a7c43e84aa51a
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-11-06 15:30:47 +0530

    codeforces Quintomania

---
+++ A_Quintomania.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> notes(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> notes[i];
+    }
+
+    bool isPerfect = true;
+    for (int i = 0; i < n - 1; i++) {
+        int interval = abs(notes[i] - notes[i + 1]);
+        if (interval != 5 && interval != 7) {
+            isPerfect = false;
+            break;
+        }
+    }
+    
+    if (isPerfect)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
